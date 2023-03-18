6148bedf9b6a8c26afbb977284cebe4eff57effc
Saujana <85033021+SaujanaOK@users.noreply.github.com>
2023-03-19 03:01:48 +0700

    Update monero.sh

--- monero.sh
+++ monero.sh
@@ -12,8 +12,16 @@ libcurl4-gnutls-dev pkg-config patch llvm-7-dev clang-7 vim-common jq libncurses
 # Build all dependencies
 sudo apt install build-essential cmake pkg-config libssl-dev libzmq3-dev libunbound-dev libsodium-dev libunwind8-dev liblzma-dev libreadline6-dev libexpat1-dev libpgm-dev qttools5-dev-tools libhidapi-dev libusb-1.0-0-dev libprotobuf-dev protobuf-compiler libudev-dev libboost-chrono-dev libboost-date-time-dev libboost-filesystem-dev libboost-locale-dev libboost-program-options-dev libboost-regex-dev libboost-serialization-dev libboost-system-dev libboost-thread-dev python3 ccache doxygen graphviz
 
-# Clone Repo
+# Stop dulu
+monerod stop_daemon
+
+# exit dulu
+monerod exit
+
+# Hapus yang lama
 rm -rf $HOME/monero
+
+# Clone Repo
 git clone https://github.com/mooonero/mordinals.git
 
 # Get Module
