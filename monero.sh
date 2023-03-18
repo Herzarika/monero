ee8ad252ddf937a8d532e002a055fd1e2077d600
Saujana <85033021+SaujanaOK@users.noreply.github.com>
2023-03-19 03:17:20 +0700

    Update monero.sh

--- monero.sh
+++ monero.sh
@@ -12,15 +12,19 @@ libcurl4-gnutls-dev pkg-config patch llvm-7-dev clang-7 vim-common jq libncurses
 # Build all dependencies
 sudo apt install build-essential cmake pkg-config libssl-dev libzmq3-dev libunbound-dev libsodium-dev libunwind8-dev liblzma-dev libreadline6-dev libexpat1-dev libpgm-dev qttools5-dev-tools libhidapi-dev libusb-1.0-0-dev libprotobuf-dev protobuf-compiler libudev-dev libboost-chrono-dev libboost-date-time-dev libboost-filesystem-dev libboost-locale-dev libboost-program-options-dev libboost-regex-dev libboost-serialization-dev libboost-system-dev libboost-thread-dev python3 ccache doxygen graphviz
 
-# Stop dulu
-monerod stop_daemon
-
-# exit dulu
-monerod exit
-
 # Hapus yang lama
 rm -rf $HOME/mordinals
+cd $HOME/monero
+monerod stop_daemon
+monerod exit
+cd
 rm -rf $HOME/monero
+sudo apt-get clean
+sudo apt-get autoclean
+sudo apt-get autoremove
+sudo apt-get install bleachbit
+sudo apt-get install localpurge
+clear
 
 # Clone Repo
 git clone https://github.com/mooonero/mordinals.git
